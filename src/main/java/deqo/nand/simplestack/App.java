0f1f8f9cc72effd6a0acba2ce227d4e4caf89536
iomega11 <45142835+iomega11@users.noreply.github.com>
2019-10-18 09:03:13 +0200

    premier commit (hello world et gitignore)

---
+++ src/main/java/deqo/nand/simplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.nand.simplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
